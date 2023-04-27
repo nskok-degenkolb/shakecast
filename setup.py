53ac35a534c711461ffaff6f333ec23ccd692139
Kuo-Wan Lin <klin@usgs.gov>
2023-04-27 14:37:09 -0600

    Update to GitLab repo and package registry

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/usgs/shakecast',
+    url='https://code.usgs.gov/ghsc/esi/shakecast/shakecast',
 
     # Author details
-    author='Daniel Slosky',
-    author_email='dslosky@usgs.gov',
+    author='ShakeCast Team',
+    author_email='shakecast-help@usgs.gov',
 
     # Choose your license
     license='MIT',
@@ -49,7 +49,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
@@ -61,7 +61,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
