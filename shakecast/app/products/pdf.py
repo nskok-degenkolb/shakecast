ccf0720646f2d82396987fd7e66cdf7f7a75f5f1
Nick Skok <71043652+nskok-degenkolb@users.noreply.github.com>
2023-09-10 13:47:48 -0700

    Update pdf.py

--- shakecast/app/products/pdf.py
+++ shakecast/app/products/pdf.py
@@ -73,7 +73,7 @@ def generate_impact_pdf(group, shakemap, save=False, pdf_name='', template_name=
         pdf.multi_cell(0, pdf.font_size, 'Confidentiality and Disclaimer Notice')
         pdf.set_font(font, '', 10)
     
-        myDisclaimer = 'This automated email message is confidential and intended soley for Providence. If received in error, please notify the sender and delete the email and any attachments from your system. The results provided herein are computer-generated building damage estimates and may not accurately represent the actual conditions or extent of damage. They are not a subsitute for a structural evaluation by a professional engineer. Degenkolb Engineers disclaims any liability for errors or omissions and makes no warranties, express or implied, regarding accuracy or completeness. Unauthorized use, disclosure, or copying is prohibited. '
+        myDisclaimer = 'This automated report is confidential and intended soley for Providence. If received in error, please notify the sender and the report from your system. The results provided herein are computer-generated building damage estimates and may not accurately represent the actual conditions or extent of damage. They are not a subsitute for a structural evaluation by a professional engineer. Degenkolb Engineers disclaims any liability for errors or omissions and makes no warranties, express or implied, regarding accuracy or completeness. Unauthorized use, disclosure, or copying is prohibited. '
         pdf.multi_cell(0, pdf.font_size, myDisclaimer)
         pdf.set_font(font, style, size)
         
