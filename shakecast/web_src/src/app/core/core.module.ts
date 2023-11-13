61342b415e8f816c1adbe13c5fded2c785808d36
lcheek <lcheek@contractor.usgs.gov>
2023-11-13 12:24:18 -0800

    Stable 9.x checkpoint

--- shakecast/web_src/src/app/core/core.module.ts
+++ shakecast/web_src/src/app/core/core.module.ts
@@ -25,7 +25,7 @@ import { LoadingService } from './loading.service';
     imports: [CommonModule]
   })
 export class CoreModule {
-static forRoot(): ModuleWithProviders {
+static forRoot(): ModuleWithProviders<CoreModule> {
     return {
     ngModule: CoreModule,
     providers: [
