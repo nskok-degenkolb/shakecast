69c36e8a4c30a896de70ff29718a9fe88ba95a82
lcheek <lcheek@contractor.usgs.gov>
2023-11-14 10:59:02 -0800

    semi-stable Angluar11 checkpoint

--- shakecast/web_src/src/app/app.routing.ts
+++ shakecast/web_src/src/app/app.routing.ts
@@ -22,4 +22,4 @@ export const appRoutingProviders: any[] = [
     AdminGuard
 ];
 
-export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes);
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' });
