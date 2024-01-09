be4f0dbcb971b176485394a2d34537efa166e9fe
Nick Skok <71043652+nskok-degenkolb@users.noreply.github.com>
2024-01-09 06:15:12 -0800

    Update pdf.py
    Add Disclaimer to template

--- shakecast/app/products/pdf.py
+++ shakecast/app/products/pdf.py
@@ -79,8 +79,9 @@ def generate_impact_pdf(group, shakemap, save=False, pdf_name='', template_name=
         pdf.set_font(font, '', 10)
         pdf.ln(pdf.font_size)
         
-        myDisclaimer = 'This automated report is confidential and intended soley for Providence. If received in error, please notify the sender and the report from your system. The results provided herein are computer-generated building damage estimates and may not accurately represent the actual conditions or extent of damage. They are not a subsitute for a structural evaluation by a professional engineer. Degenkolb Engineers disclaims any liability for errors or omissions and makes no warranties, express or implied, regarding accuracy or completeness. Unauthorized use, disclosure, or copying is prohibited. '
-        pdf.multi_cell(0, pdf.font_size, myDisclaimer)
+        #myDisclaimer = 'This automated report is confidential and intended soley for Providence. If received in error, please notify the sender and the report from your system. The results provided herein are computer-generated building damage estimates and may not accurately represent the actual conditions or extent of damage. They are not a subsitute for a structural evaluation by a professional engineer. Degenkolb Engineers disclaims any liability for errors or omissions and makes no warranties, express or implied, regarding accuracy or completeness. Unauthorized use, disclosure, or copying is prohibited. '
+        
+        pdf.multi_cell(0, pdf.font_size, configs['disclaimer'].get('disclaimer_text','Disclaimer'))
         pdf.set_font(font, style, size)
         
     except:
